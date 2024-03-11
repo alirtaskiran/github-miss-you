e9935506d6ecbca53d991d3e457bca002af9e662
alirtaskiran <alirtaskiran@gmail.com>
2024-03-12 02:56:30 +0300

    changed all build

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-fetch("https://api.github.com/users/alirtaskiran/repos")
-  .then((response) => response.json())
-  .then((data) => {
+const url = "https://api.github.com/users";
 
-    data.forEach((repo) => {
-      console.log(repo.pushed_at);
-    
-  })
-})
-  .catch((error) => console.error(error));
+async function getUser(username) {
+  let response = await fetch(url + `/${username}`);
+  let data = await response.json();
+  return data;
+}
+
+getUser("alirtaskiran").then(data => console.log(data));
