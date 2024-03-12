a4c30895d014ee8f73552c9d238b72072ee5e21a
alirtaskiran <alirtaskiran@gmail.com>
2024-03-12 14:38:10 +0300

    data accessed (pushed_at)

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ async function getUser(username) {
 }
 
 getUser("alirtaskiran").then(data => console.log(data));
+
+async function getRepos(username) {
+  let response = await fetch(url + `/${username}/repos`);
+  let data = await response.json();
+  return data;
+}
+
+getRepos("alirtaskiran").then(
+  data => data.forEach(repo => console.log(repo.pushed_at))
+);
